4f9fbb43c7c095884cd079c60a9d44c67fe0654b
Sankalp Khanna <136170522+Sankalp2114@users.noreply.github.com>
2024-04-03 16:06:00 +0530

    added vercel analytics

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import { ReactNode } from "react";
 import "../globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "DevConnect",
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <body className={`${inter.className} bg-dark-1`}>
           <div className="w-full flex justify-center items-center min-h-screen">
             {children}
+            <Analytics />
           </div>
         </body>
       </html>
